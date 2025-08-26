f7c0229e30165f3f0fe2de65e0387800b4c94ae2
Bandile-1 <owuormichael40@gmail.com>
2025-08-26 19:20:31 +0300

    Add files via upload

---
+++ zigzag.py
@@ -0,0 +1,23 @@
+from textwrap import indent
+import time, sys
+
+indent = 0
+indentIncreasing = True
+
+try:
+    while True:
+        print('' * indent, end='')
+        print('********')
+        time.sleep(0.5)
+
+        if indentIncreasing:
+            indent+=1
+            if indent == 20:
+                indentIncreasing = False
+        else:
+            indent-+1
+            if indent == 0:
+                indentIncreasing = True
+
+except KeyboardInterrupt:
+    sys.exit()
